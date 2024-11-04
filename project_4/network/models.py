7bb90ee0f27e27eaea1f1ae942705317a9a1c188
qristaan terry <qristaanterry@gmail.com>
2024-11-03 19:55:17 -0800

    version before first migration yes

--- project_4/network/models.py
+++ project_4/network/models.py
@@ -13,34 +13,40 @@ class User(AbstractUser):
     def __str__(self):
         return f"User = {self.username}" 
 
-class Post(models.Model):
-    # poster = models.ForeignKey(User, on_delete=models.CASCADE)
-    # likes = models.IntegerField(default=0)
-    # timestamp = models.DateTimeField(auto_now_add=True)
-    pass
 
+class Post(models.Model):
+    poster = models.ForeignKey(User, on_delete=models.CASCADE)
+    likes = models.IntegerField(default=0)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+
+class Following(models.Model):
+
+    # person A is following person B ...
+    follower = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    # person B is being followed by person A
+    following = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    # set restriction so that followers and following ...
+    # ... cannot be the same person
+
+    def valid_following(self):
+        # return true if following and follower are not same person, else return false
+        return self.follower != self.following
+    
+    class meta:
+        unique_together = ('follower', 'following')
+    
+
+class Comment(models.Model):
+    commenter = models.ForeignKey(User, on_delete=models.CASCADE)
+    comment = models.CharField(max_length=1000)
+    post_commented_on = models.ForeignKey(Post, on_delete=models.CASCADE)
+    
 # class Profile(models.Model):
 #     pass
 #     # user
 #     # followers
 ####  Might not need this model?
     
-
-class Following(models.Model):
-    # follower
-    # following
-
-    # set restriction so that followers and following ...
-    # ... cannot be the same person
-    pass
-
-class Follower(models.Model):
-    # following
-    # follower
-    pass
-
-class Comment(models.Model):
-    # commenter
-    # comment
-    # post commented on
-    pass
